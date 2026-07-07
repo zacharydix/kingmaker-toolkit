5d7789a01cf1aea64fbb54aebbe7794dcb1d3496
Zachary Dix <zach47d@gmail.com>
2026-07-07 13:28:31 -0500

    Migrates kingdom event to module

---
+++ scripts/data/kingdom-event-data.js
@@ -0,0 +1,9 @@
+export const KINGDOM_EVENT_CONFIG = {
+  startingDC: 16,
+  dcDecrease: 5,
+  minimumDC: 0,
+  rollFormula: '1d20',
+  tableName: 'Random Kingdom Events',
+  flagScope: 'world',
+  flagKey: 'decreasingFlatCheckDC',
+};
